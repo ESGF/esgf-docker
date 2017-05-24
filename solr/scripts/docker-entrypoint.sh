e5f209a94bde3ed2b37491618baaa7b227e13b60
Luca Cinquini <cinquini@LMC-038261.local>
2017-05-24 09:28:22 -0700

    Starting Solr with supervisor

--- solr/scripts/docker-entrypoint.sh
+++ solr/scripts/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# script to start one Solr instance per shard
+# Script to start one Solr instance per shard
 # and keep the Docker container running
 
-cd $SOLR_INSTALL_DIR/bin
-export SOLR_INCLUDE=${SOLR_HOME}/solr.in.sh
-./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/master-8984 -p 8984 -a '-Denable.master=true'
-./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/slave-8983 -p 8983 -a '-Denable.slave=true -Denable.master=true' 
+# start supervisor --> solr shards
+supervisord -c /etc/supervisord.conf
 
+# print out log file
+sleep 2
 tail -f $SOLR_INSTALL_DIR/server/logs/solr.log
