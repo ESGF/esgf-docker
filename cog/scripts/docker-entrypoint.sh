815bdf528d0ade855199a420846da0bdb726b779
Luca Cinquini <cinquini@LMC-038261.local>
2017-07-14 03:54:11 -0600

    Separating CoG initialization in different script.

--- cog/scripts/docker-entrypoint.sh
+++ cog/scripts/docker-entrypoint.sh
@@ -14,22 +14,14 @@ echo "ESGF_FLAG=$ESGF_FLAG"
 export RUNSERVER=$3
 echo "RUNSERVER=$RUNSERVER"
 
-# use virtualenv
-source $COG_DIR/venv/bin/activate
-
-# upgrade CoG
-cd $COG_INSTALL_DIR
-python setup.py -q setup_cog --esgf=$ESGF_FLAG
-
-# customize CoG settings
-echo "Using ESGF_HOSTNAME=$ESGF_HOSTNAME"
-sed -i 's/ALLOWED_HOSTS = .*/ALLOWED_HOSTS = '"${ESGF_HOSTNAME}"'/g' $COG_CONFIG_DIR/cog_settings.cfg
-
-# FIXME:  PRODUCTION_SERVER = True would require use of SSL to transmit any cookie
-sed -i 's/PRODUCTION_SERVER = True/PRODUCTION_SERVER = False/g' $COG_CONFIG_DIR/cog_settings.cfg
+# execute CoG initialization
+if [ $INIT == "true" ]; then
+   echo "Executing CoG initialization..."
+   scriptdir=`dirname "$BASH_SOURCE"`
+   $scriptdir/docker-init.sh $ESGF_HOSTNAME $ESGF_FLAG
+fi
 
 # start django server in virtual environment
-# or keep the container running
 if [ $RUNSERVER == "true" ]; then
    echo "Starting CoG server through supervisor daemon"
    # start supervisor --> cog
