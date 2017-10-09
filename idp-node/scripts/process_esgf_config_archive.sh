a4d6ed7868ed60f4bbd2ea837fb203be62038fd4
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-10-09 18:42:02 +0200

    turn idp $ESGF_CONFIG into secret

---
+++ idp-node/scripts/process_esgf_config_archive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "untar esgf config files"
+mkdir -p /esg
+tar --same-owner -pxaf /root/archives/esgf_config.tar.xz -C /esg
+
+echo "set permissions"
+chmod 644 /esg/config/*
+chown root:tomcat /esg/config/.esg*
+chmod 640 /esg/config/.esg*
+chmod 755 /esg/config/esgcet
+chown -R tomcat:tomcat /esg/config/tomcat
+chmod 755 /esg/config/tomcat
+chmod 644 /esg/config/esgcet/*
+chmod 600 /esg/config/tomcat/*
