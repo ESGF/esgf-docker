b4c160a9c916a6fa6703e454a490b65c6e419237
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-10-11 16:52:39 +0200

    disable owner modifications for tomcat files on images that don't have gui&uid tomcat

--- httpd/scripts/process_esgf_config_archive.sh
+++ httpd/scripts/process_esgf_config_archive.sh
@@ -6,10 +6,10 @@ tar --same-owner -pxaf /root/archives/esgf_config.tar.xz -C /esg
 
 echo "set permissions"
 chmod 644 /esg/config/*
-chown root:tomcat /esg/config/.esg*
+#chown root:tomcat /esg/config/.esg*
 chmod 640 /esg/config/.esg*
 
-chown -R tomcat:tomcat /esg/config/tomcat
+#chown -R tomcat:tomcat /esg/config/tomcat
 chmod 755 /esg/config/tomcat
 chmod 600 /esg/config/tomcat/*
 
