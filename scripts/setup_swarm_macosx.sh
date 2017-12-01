43a1f1bfad0bf60cc250ad877fd71675c39fa330
Luca Cinquini <cinquini@ps-128-149-80-206.jpl.nasa.gov>
2017-12-01 06:26:36 -0700

    Adding aws/EsgfSwarmManager.config

--- scripts/setup_swarm_macosx.sh
+++ scripts/setup_swarm_macosx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 
-# Script to setup a Docker Swarm composed of 6 nodes on a MacOSX laptop.
+# Script to setup a Docker Swarm composed of 5 nodes on a MacOSX laptop.
 
 # create all VMs
 # (assign more memory to last node which will be the data-node)
@@ -8,8 +8,7 @@ docker-machine create --driver virtualbox node1
 docker-machine create --driver virtualbox node2
 docker-machine create --driver virtualbox node3
 docker-machine create --driver virtualbox node4
-docker-machine create --driver virtualbox node5
-docker-machine create --driver virtualbox --virtualbox-memory 2048 node6
+docker-machine create --driver virtualbox --virtualbox-memory 2048 node5
 docker-machine ls
 
 # start the swarm
@@ -23,7 +22,7 @@ token_manager=`docker swarm join-token --quiet manager`
 #docker node update --availability drain node1
 
 # join the swarm
-for i in `seq 2 6`;
+for i in `seq 2 5`;
 do
    eval $(docker-machine env node$i)
    docker swarm join --token $token_worker $MANAGER_IP:2377
@@ -35,6 +34,6 @@ docker node ls
 docker node update --label-add esgf_front_node=true node1
 docker node update --label-add esgf_db_node=true node2
 docker node update --label-add esgf_index_node=true node3
+docker node update --label-add esgf_solr_node=true node3
 docker node update --label-add esgf_idp_node=true node4
-docker node update --label-add esgf_solr_node=true node5
-docker node update --label-add esgf_data_node=true node6
+docker node update --label-add esgf_data_node=true node5
