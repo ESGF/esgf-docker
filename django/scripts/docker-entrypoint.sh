2f50c3d9137b65b1c5f357bea652c1a345521614
Matt Pryor <matt.pryor@stfc.ac.uk>
2018-01-24 10:49:52 +0000

    SLCS is working

---
+++ django/scripts/docker-entrypoint.sh
@@ -0,0 +1,99 @@
+#! /usr/bin/env bash
+
+set -x
+
+# Configure settings module to the first argument
+export DJANGO_SETTINGS_MODULE="$1"
+
+# Execute customisations from /django-init.d before doing anything
+if [ -d "/django-init.d" ]; then
+    for file in $(find /django-init.d/ -mindepth 1 -type f -executable | sort -n); do
+        # All customisations have access to the exported environment variables only,
+        # whether they are bash, python or otherwise
+        eval $file || exit 1
+    done
+fi
+
+# Run database migrations
+django-admin migrate --no-input || exit 1
+
+# Create Django superuser if required
+if [ "${DJANGO_CREATE_SUPERUSER:-0}" -eq 1 ]; then
+    # We require that username and email exist
+    if [ -z "$DJANGO_SUPERUSER_USERNAME" ]; then
+        echo "[ERROR] DJANGO_SUPERUSER_USERNAME must be set to create superuser" 1>&2
+        exit 1
+    fi
+    if [ -z "$DJANGO_SUPERUSER_EMAIL" ]; then
+        echo "[ERROR] DJANGO_SUPERUSER_EMAIL must be set to create superuser" 1>&2
+        exit 1
+    fi
+    # This command exits with a non-zero exit code if the user already exists
+    django-admin shell <<STDIN
+import sys
+from django.contrib.auth import get_user_model
+
+if get_user_model().objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    sys.exit(1)
+STDIN
+    # A zero exit status means the user needs to be created
+    if [ "$?" -eq 0 ]; then
+        # Create the superuser with an unusable password
+        django-admin createsuperuser --no-input  \
+                                     --username "$DJANGO_SUPERUSER_USERNAME"  \
+                                     --email "$DJANGO_SUPERUSER_EMAIL"  \
+                                     $DJANGO_SUPERUSER_EXTRA_ARGS || exit 1
+        # Update the password for the superuser if required
+        # For compatability with Docker secrets, which can only be mounted as
+        # files, allow for DJANGO_SUPERUSER_PASSWORD_FILE as well
+        if [ -z "$DJANGO_SUPERUSER_PASSWORD" ] && [ -f "$DJANGO_SUPERUSER_PASSWORD_FILE" ]; then
+            DJANGO_SUPERUSER_PASSWORD=$(cat "$DJANGO_SUPERUSER_PASSWORD_FILE")
+        fi
+        if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+            django-admin shell <<STDIN || exit 1
+from django.contrib.auth import get_user_model
+
+user = get_user_model().objects.get(username='$DJANGO_SUPERUSER_USERNAME')
+user.set_password('$DJANGO_SUPERUSER_PASSWORD')
+user.save()
+STDIN
+        fi
+    fi
+fi
+
+# Collect static files for serving later
+django-admin collectstatic --no-input --clear || exit 1
+
+# Create the Paste config file
+# Note that we have to do this rather than using Paste variables because we want
+# to have a dynamic route name in the urlmap
+function django_setting {
+    setting=$(django-admin shell <<< "from django.conf import settings; print(settings.$1)" || exit 1)
+    # Remove any instances of the REPL string and any remaining whitespace
+    echo -e "${setting//">>>"/}" | tr -d '[:space:]'
+}
+cat > /home/gunicorn/paste.ini <<EOF
+[composite:main]
+use = egg:Paste#urlmap
+/ = django
+$(django_setting STATIC_URL) = static
+
+[app:django]
+use = call:django_paste:app_factory
+django_wsgi_application = $(django_setting WSGI_APPLICATION)
+
+[app:static]
+use = egg:Paste#static
+document_root = $(django_setting STATIC_ROOT)
+
+[server:main]
+use = egg:gunicorn#main
+EOF
+
+# Run the app with gunicorn
+exec gunicorn --paste /home/gunicorn/paste.ini \
+              --bind 0.0.0.0:${GUNICORN_PORT:-8000} \
+              --access-logfile '-' \
+              --error-logfile '-' \
+              --log-level ${GUNICORN_LOG_LEVEL:-info} \
+              --workers ${GUNICORN_WORKERS:-4}
---
+++ slcs/conf/settings.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Django settings for the ESGF SLCS Server.
+"""
+
+import os
+
+from django_env_settings import *
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'oauth2_provider',
+    'bootstrap3',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'esgf_slcs_server.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'esgf_slcs_server', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'esgf_slcs_server.wsgi.application'
+
+ESGF_USERDB_USER_SCHEMA = os.environ.get('ESGF_USERDB_USER_SCHEMA', 'esgf_security')
+ESGF_USERDB_USER_TABLE = os.environ.get('ESGF_USERDB_USER_TABLE', 'user')
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+AUTHENTICATION_BACKENDS = [
+    'oauth2_provider.backends.OAuth2Backend',
+    'esgf_auth.backend.EsgfUserBackend'
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'esgf_slcs_server', 'static')]
+
+
+# URLs for login/logout
+LOGIN_URL = 'accounts_login'
+LOGOUT_URL = 'accounts_logout'
+LOGIN_REDIRECT_URL = 'home'
+
+
+BOOTSTRAP3 = {
+    'success_css_class': '',
+}
+
+
+# OAuth provider configuration
+CERTIFICATE_SCOPE = '{}/oauth/certificate/'.format(os.environ['ESGF_SLCS_ROOT_URL'])
+OAUTH2_PROVIDER = {
+    'SCOPES' : {
+        CERTIFICATE_SCOPE : 'Obtain short-lived certificate for user',
+    },
+    'DEFAULT_SCOPES' : [CERTIFICATE_SCOPE],
+}
+
+BASIC_AUTH_REALM = os.environ['ESGF_SLCS_BASIC_AUTH_REALM']
+
+
+# Configuration for the Online CA WSGI application
+# This is instantiated using the PasteDeploy app_factory, so the configuration below
+# corresponds to the global and local configs expected by that function
+ONLINECA_PASTEDEPLOY_CONF = 'config:{0}'.format(os.environ['ONLINECA_PASTEDEPLOY_FILE'])
+ONLINECA_DJANGO_USER_TO_OPENID = 'esgf_auth.openid.django_user_to_openid'
