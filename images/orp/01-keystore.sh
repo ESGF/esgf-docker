9451e3f1cca6e68b9e6529bf54860cc161e6b4d6
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-03-05 09:50:26 +0000

    Try using environment variables in properties file

--- images/orp/01-keystore.sh
+++ images/orp/01-keystore.sh
@@ -10,27 +10,29 @@ set -eo pipefail
 # Create a temporary file for openssl to put random state
 export RANDFILE="$(mktemp)"
 
-KEYSTORE_ALIAS="${ESGF_KEYSTORE_ALIAS:-esgf-self}"
-KEYSTORE_FILE="$ESGF_HOME/tomcat/hostcert.p12"
+export ESGF_KEYSTORE_ALIAS="${ESGF_KEYSTORE_ALIAS:-esgf-self}"
+export ESGF_KEYSTORE_FILE="${ESGF_KEYSTORE_FILE:-$ESGF_HOME/tomcat/hostcert.p12}"
 # Generate a random keystore password for this container run
-KEYSTORE_PASSWORD="$(openssl rand -hex 32)"
+export ESGF_KEYSTORE_PASSWORD="$(openssl rand -hex 32)"
+
+ESGF_HOSTCERT_CERT_FILE="${ESGF_HOSTCERT_CERT_FILE:-$ESGF_HOME/hostcert/tls.crt}"
+ESGF_HOSTCERT_KEY_FILE="${ESGF_HOSTCERT_KEY_FILE:-$ESGF_HOME/hostcert/tls.key}"
 
 # Create the keystore
 echo "[info] Creating PKCS12 bundle for host certificate and key"
-mkdir -p "$(dirname "$ESGF_KEYSTORE_FILE")"
 openssl pkcs12 -export \
-    -name "$KEYSTORE_ALIAS" \
-    -out "$KEYSTORE_FILE" \
-    -in "$ESGF_HOSTCERT_DIR/tls.crt" \
-    -inkey "$ESGF_HOSTCERT_DIR/tls.key" \
-    -password "pass:$KEYSTORE_PASSWORD"
+    -name "$ESGF_KEYSTORE_ALIAS" \
+    -out "$ESGF_KEYSTORE_FILE" \
+    -in "$ESGF_HOSTCERT_CERT_FILE" \
+    -inkey "$ESGF_HOSTCERT_KEY_FILE" \
+    -password "pass:$ESGF_KEYSTORE_PASSWORD"
 
 rm -rf "$RANDFILE"
 unset RANDFILE
 
 # Configure the ORP to use it
-echo "[info] Configuring ORP to use PKCS12 bundle"
-CATALINA_EXTRA_OPTS="-Desg.orp.keystore.file=$KEYSTORE_FILE"
-CATALINA_EXTRA_OPTS="$CATALINA_EXTRA_OPTS -Desg.orp.keystore.alias=$KEYSTORE_ALIAS"
-CATALINA_EXTRA_OPTS="$CATALINA_EXTRA_OPTS -Desg.orp.keystore.password=$KEYSTORE_PASSWORD"
-export CATALINA_EXTRA_OPTS
+# echo "[info] Configuring ORP to use PKCS12 bundle"
+# CATALINA_EXTRA_OPTS="-Desg.orp.keystore.file=$KEYSTORE_FILE"
+# CATALINA_EXTRA_OPTS="$CATALINA_EXTRA_OPTS -Desg.orp.keystore.alias=$KEYSTORE_ALIAS"
+# CATALINA_EXTRA_OPTS="$CATALINA_EXTRA_OPTS -Desg.orp.keystore.password=$KEYSTORE_PASSWORD"
+# export CATALINA_EXTRA_OPTS
