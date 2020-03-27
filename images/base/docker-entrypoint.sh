bfa5b153f3c611cfaab853fe462f9679073c6aed
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-03-27 16:06:55 +0000

    Allow any user in the tomcat group to run the container

--- images/base/docker-entrypoint.sh
+++ images/base/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 
 # Look for scripts in the init directory and execute them in lexicograhpical order
 # Bash scripts are sourced, any other executable script is just executed
-echo "[info] Running pre-init customisations from /docker-entrypoint.d"
-if [ -d "/docker-entrypoint.d" ]; then
-    for file in $(find /docker-entrypoint.d -mindepth 1 -type f -executable | sort -n); do
+echo "[info] Running pre-init customisations from /docker-init.d"
+if [ -d "/docker-init.d" ]; then
+    for file in $(find /docker-init.d -mindepth 1 -type f -executable | sort -n); do
         echo "[info] Running customisations from $file"
         case "$file" in
             *.sh) source $file ;;
