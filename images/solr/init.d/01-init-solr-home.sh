1a999819d0cafac294f7826405d766565dc459e1
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-07-07 15:54:35 +0100

    Add ESGF core templates + core initialisation

--- images/solr/init.d/01-init-solr-home.sh
+++ images/solr/init.d/01-init-solr-home.sh
@@ -8,8 +8,8 @@ set -eo pipefail
 
 
 # First, make sure the directory exists and is writable
-test -d $SOLR_HOME || (echo "[error] $SOLR_HOME does not exist" 2>&1; exit 1)
-test -w $SOLR_HOME || (echo "[error] $SOLR_HOME is not writable by $(id -u):$(id -g)" 2>&1; exit 1)
+test -d "$SOLR_HOME" || (echo "[error] $SOLR_HOME does not exist" 2>&1; exit 1)
+test -w "$SOLR_HOME" || (echo "[error] $SOLR_HOME is not writable by $(id -u):$(id -g)" 2>&1; exit 1)
 
 # Copy files from /opt/solr that are required
 test -r "$SOLR_HOME/solr.xml" || cp -a /opt/solr/server/solr/solr.xml "$SOLR_HOME/solr.xml"
