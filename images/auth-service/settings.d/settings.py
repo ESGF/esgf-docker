4580bcca54f46b9c926753434f644d54e9d98aab
William Tucker <william.tucker@stfc.ac.uk>
2020-10-07 13:07:03 -0400

    Cleanup up auth service settings

--- images/auth-service/settings.d/settings.py
+++ images/auth-service/settings.d/settings.py
@@ -23,28 +23,4 @@ WSGI_APPLICATION = 'auth_service.wsgi.application'
 
 # Use a non database session engine
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-
-# Authlib settings
-
-OAUTH_CLIENT_ID = ""
-OAUTH_CLIENT_SECRET = ""
-OAUTH_TOKEN_URL = ""
-OAUTH_TOKEN_INTROSPECT_URL = ""
-
-OIDC_BACKEND_CLIENT_NAME = "mybackend"
-AUTHLIB_OAUTH_CLIENTS = {
-    OIDC_BACKEND_CLIENT_NAME: {
-        "client_id": OAUTH_CLIENT_ID,
-        "client_secret": OAUTH_CLIENT_SECRET,
-        "authorize_url": "",
-        "userinfo_endpoint": "",
-        "server_metadata_url": "",
-        "client_kwargs": {"scope": "openid profile email"}
-    }
-}
-
-# Athorization settings
-
-RESOURCE_SERVER_URI = ""
-ATTRIBUTE_SERVICE_URL = ""
-AUTHORIZATION_SERVICE_URL = ""
+SESSION_COOKIE_SECURE = False
