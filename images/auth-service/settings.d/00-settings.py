dfc9f71f2fcf5b2423ecd94005e50b1847602f66
William Tucker <william.tucker@stfc.ac.uk>
2021-11-01 17:35:05 +0000

    Added optional authz exemption function for deployments

--- images/auth-service/settings.d/00-settings.py
+++ images/auth-service/settings.d/00-settings.py
@@ -1,3 +1,10 @@
+import re
+
+from django.conf import settings
+from urllib.parse import urlparse
+
+from authenticate.utils import get_requested_resource
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -12,3 +19,20 @@ WSGI_APPLICATION = 'auth_service.wsgi.application'
 # Use a non database session engine
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 SESSION_COOKIE_SECURE = False
+
+# Authorization bypass function for exempt paths
+def is_exempt(request):
+
+    if not settings.AUTHORIZATION_EXEMPT_PATHS:
+        return False
+
+    resource = get_requested_resource(request)
+    path = urlparse(resource).path.lstrip("/")
+
+    # Check exempt path patterns against requested URL
+    for expr in settings.AUTHORIZATION_EXEMPT_PATHS:
+        if re.compile(expr).match(path):
+            return True
+
+AUTHORIZATION_EXEMPT_PATHS = [] # e.g. [".*"]
+AUTHORIZATION_EXEMPT_FILTER = is_exempt
