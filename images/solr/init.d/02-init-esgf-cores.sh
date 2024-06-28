3eee644a0582c2fb9b6d09bd75abefc12d60b87c
William Tucker <william.tucker@stfc.ac.uk>
2024-06-28 15:48:52 +0100

    Fixed an issue with the SOLR properties file

--- images/solr/init.d/02-init-esgf-cores.sh
+++ images/solr/init.d/02-init-esgf-cores.sh
@@ -10,11 +10,11 @@ ESGF_CORES=( "datasets"  "files"  "aggregations" )
 for corename in "${ESGF_CORES[@]}"; do
     coredir="$SOLR_HOME/$corename"
     # Create the core directory if it doesn't already exist
-    if [ ! -f "$coredir/core.properties" ]; then
+    if [ ! -d "$coredir" ]; then
         mkdir -p "$coredir"
-        touch "$coredir/core.properties"
     fi
     # Replace the core configuration with the most recent version
+    cp "/esg/solr-home/$corename/core.properties" "$coredir/core.properties"
     rm -rf "$coredir/conf"
     cp -r /esg/core-template/conf "$coredir/conf"
 done
